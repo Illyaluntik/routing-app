e82aeeb26eeef27bd6c3b7496606dcdd3942df61
Illia Lynnyk <illyalinnyk@gmail.com>
2025-09-16 16:03:26 -0400

    Updates

--- assets/PointSnappingHint-DqEsmnFd.js
+++
@@ -1,1 +0,0 @@
-import{dD as i}from"./index-DovH5zcQ.js";import{a as n}from"./hitTestSelectUtils-oy8CWiGh.js";class o extends n{constructor(t,s,r){super(s,r),this.point=t}equals(t){return t instanceof o&&i(this.point,t.point)}}export{o as n};
